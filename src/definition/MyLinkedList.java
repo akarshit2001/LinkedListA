e6a48a15eedc869eda46dc083662088868ecee04
loser <loser>
2020-01-23 11:05:17 +0530

    add constructor for data and next field

--- src/definition/MyLinkedList.java
+++ src/definition/MyLinkedList.java
@@ -11,6 +11,11 @@ public class MyLinkedList<E> implements LinkedListADT<E> {
         private Node(E data) {
             this.data = data;
         }
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
     }
 
     @Override
