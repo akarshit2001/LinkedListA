47727f11bbd010e7f191083205a4c70a0cdc3a35
loser <loser>
2020-01-24 23:11:49 +0530

    override methods of LinkedListADT interface

--- src/definition/MyLinkedList.java
+++ src/definition/MyLinkedList.java
@@ -17,6 +17,26 @@ public class MyLinkedList<E> implements LinkedListADT<E> {
 
     }
 
+    @Override
+    public E remove(E item) {
+        return null;
+    }
+
+    @Override
+    public int search(E item) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
+
     private static class Node<E> {
         private E data;
         private Node<E> next;
